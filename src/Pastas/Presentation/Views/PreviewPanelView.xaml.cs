5c2dd24d68e9c53e45c75a73ebffcd0ab4f89e3e
synsobaki <aikhal.gogolev@mail.ru>
2026-05-05 07:55:43 +0900

    PR-10 compact UI interaction polish

--- src/Pastas/Presentation/Views/PreviewPanelView.xaml.cs
+++ src/Pastas/Presentation/Views/PreviewPanelView.xaml.cs
@@ -1,12 +1,50 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Pastas.Presentation.ViewModels;
 
 namespace Pastas.Presentation.Views;
 
-public partial class PreviewPanelView : System.Windows.Controls.UserControl
+public partial class PreviewPanelView : UserControl
 {
+    private bool _isCopyFeedbackActive;
+
     public PreviewPanelView()
     {
         InitializeComponent();
     }
-}
 
+    private async void CopyButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (DataContext is not MainViewModel viewModel || viewModel.SelectedItem is null)
+        {
+            return;
+        }
+
+        if (!viewModel.CopyItemCommand.CanExecute(viewModel.SelectedItem))
+        {
+            return;
+        }
+
+        viewModel.CopyItemCommand.Execute(viewModel.SelectedItem);
+
+        if (_isCopyFeedbackActive)
+        {
+            return;
+        }
+
+        _isCopyFeedbackActive = true;
+        CopyButton.Content = "Copied";
+
+        try
+        {
+            await Task.Delay(TimeSpan.FromMilliseconds(1200));
+        }
+        finally
+        {
+            CopyButton.Content = "Copy";
+            _isCopyFeedbackActive = false;
+        }
+    }
+}
